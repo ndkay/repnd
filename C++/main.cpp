19798abf35e4bcd3ad3eaecbe3e3aab82b91214b
vlad <kozlyakovskiy00@mail.ru>
2020-09-21 12:48:40 +0300

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	   
+	int g;
+	g = 2;
+	c = (a+b)*g;
+cout<<c<<endl;	 
 	
-	cout<<"hello world"<<endl;
+	
 	
 	system("pause");
 	return 0;
