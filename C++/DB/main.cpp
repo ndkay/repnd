69d4b777f8f7c0811831a58635afe9c55780d394
vlados <47642760+ndkay@users.noreply.github.com>
2020-09-25 12:34:11 +0300

    DBj

--- C++/DB/main.cpp
+++ C++/DB/main.cpp
@@ -10,42 +10,43 @@ int main(int argc, char** argv) {
 	
 setlocale(LC_ALL,"russian");
 	string bazanames[256] = {"Козляковский В.О.","Кормщиков И.В.","Филиппов В.Д"};
-	cout<<"Взломать базу данных?"<<endl;
+	cout<<"Hack the base?"<<endl;
+		cout<<"Yes or No"<<endl;
 	string k;
 	cin>>k;
 	
 	
-	if(k=="Yes"){
+	if(k=="Yes"||k=="yes"||k=="YES"||k=="yEs"||k=="yeS"){
 		while(t<100){
 			for(t = 0.1;t<=100;t++){
 				cout<<t<<" %";
 				system("cls");
-				cout<<"Взлом базы...";
-				
+				cout<<"Hacking...";
 				
 			}
 	
 	}
-		cout<<"Хотите получить список базы?"<<endl;
+		cout<<"Want to get a list of names in the database?"<<endl;
+		cout<<"Yes or No"<<endl;
 		string h;
 		cin>>h;
-		if(h=="Yes"){
+		if(h=="Yes"||k=="yes"||k=="YES"||k=="yEs"||k=="yeS"){
 			for(int i = 0;i<20;i++){
 				cout<<bazanames[i]<<endl;
 			}
 		}
-		else if(h=="No"){
-			cout<<"Вы покидаете ббазу"<<endl;
-			return 0;
-		}
-	
 			
 		else{
-			cout<<"Вы покидаете ббазу"<<endl;
+			cout<<"You leave base"<<endl;
 			return 0;
 		}
+		
 	}
+	else{
+			cout<<"You leave base"<<endl;
+			return 0;
+		}
 	
-
+system("pause");
     return 0;
 }
