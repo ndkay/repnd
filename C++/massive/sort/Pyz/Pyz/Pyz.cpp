41edbc6be33a0044d64b7ab18538832b5f1b10e7
vlados <47642760+ndkay@users.noreply.github.com>
2020-09-30 13:03:40 +0300

    sortirovka

---
+++ C++/massive/sort/Pyz/Pyz/Pyz.cpp
@@ -0,0 +1,56 @@
+﻿// Pyz.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+void puzFunc() {
+    int arr[5] = { 1,3,4,8,4 };
+
+    int temp;
+
+
+    for (int i = 0; i < 5 - 1; i++) {
+        for (int j = 0; j < 5 - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+};
+
+
+int main()
+{
+    bool tr = true;
+    while(tr) {
+        cout << "1. Puzyrkovaya sortirovka\n2. Exit" << endl;
+        int a;
+        cin >> a;
+        switch (a) {
+        case 1: {
+            puzFunc();
+            break;
+        }
+        case 2: {
+            return 0;
+            break;
+
+        }
+        }
+
+
+    }
+}
